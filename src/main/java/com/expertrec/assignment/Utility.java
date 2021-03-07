c6e1cc53239160b253f904a79bc6fe36ded0a9db
Subhadeep Majumder <m.subhadeep2707@gmail.com>
2021-03-07 16:46:37 +0530

    added method to check for html response

--- src/main/java/com/expertrec/assignment/Utility.java
+++ src/main/java/com/expertrec/assignment/Utility.java
@@ -1,11 +1,17 @@
 package com.expertrec.assignment;
 
 import org.apache.commons.validator.routines.UrlValidator;
+import org.apache.http.HttpResponse;
 
 public class Utility {
     boolean validateUrl(String url){
         String[] schemes = {"http","https"}; // DEFAULT schemes = "http", "https", "ftp"
         UrlValidator urlValidator = new UrlValidator(schemes);
         return urlValidator.isValid(url);
+    }
+
+    boolean checkIfHTMLContent(HttpResponse response) {
+        String server = response.getFirstHeader("Content-Type").getValue();
+        return server.contains("text/html");
     }
 }
