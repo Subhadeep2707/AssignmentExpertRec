17e6983faa51f76447f202f57409a214ae931bec
Subhadeep Majumder <m.subhadeep2707@gmail.com>
2021-03-07 16:21:13 +0530

    added utility class

--- src/main/java/com/expertrec/assignment/Application.java
+++ src/main/java/com/expertrec/assignment/Application.java
@@ -2,4 +2,15 @@ package com.expertrec.assignment;
 
 public class Application {
 
+    private String file;
+    private String url;
+
+    Application(String url, String file){
+        this.url = url;
+        this.file = file;
+    }
+
+    void handler(){
+
+    }
 }
--- src/main/java/com/expertrec/assignment/Main.java
+++ src/main/java/com/expertrec/assignment/Main.java
@@ -3,7 +3,10 @@ package com.expertrec.assignment;
 public class Main {
     public static void main(String[] args) {
 
-        Application application = new Application();
+        String urlArg = "https://www.google.com";
+        String fileArg = "google.txt";
+
+        Application application = new Application(urlArg, fileArg);
 
     }
 }
